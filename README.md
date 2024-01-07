b50c412aecd929efc75db2ddfabdad680546606f
Guo Yunhe <i@guoyunhe.me>
2024-01-07 22:54:11 +0800

    use remix icon

--- README.md
+++ README.md
@@ -18,3 +18,12 @@ import { Playground } from '@rive/doc-ui';
 
 <Playground code="<div>Hello, World!</div>" />;
 ```
+
+## Acknowledge
+
+This project is based on the following open source projects:
+
+- [react-live](https://github.com/FormidableLabs/react-live) and [prism-react-renderer](https://github.com/FormidableLabs/prism-react-renderer) by [FormidableLabs](https://github.com/FormidableLabs)
+- [react](https://reactjs.org)
+- [remixicon](https://github.com/Remix-Design/RemixIcon)
+- [vite](https://vitejs.dev)
--- src/private/SuccessButton.tsx
+++ src/private/SuccessButton.tsx
@@ -1,3 +1,4 @@
+import { RiCheckboxCircleLine } from '@remixicon/react';
 import { ToolButton } from './ToolButton';
 
 export interface SuccessButtonProps {
@@ -6,15 +7,7 @@ export interface SuccessButtonProps {
 
 export function SuccessButton({ locale }: SuccessButtonProps) {
   return (
-    <ToolButton
-      color="success"
-      icon={
-        <svg fill="currentColor" viewBox="0 0 16 16">
-          <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14m0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16" />
-          <path d="m10.97 4.97-.02.022-3.473 4.425-2.093-2.094a.75.75 0 0 0-1.06 1.06L6.97 11.03a.75.75 0 0 0 1.079-.02l3.992-4.99a.75.75 0 0 0-1.071-1.05" />
-        </svg>
-      }
-    >
+    <ToolButton color="success" icon={<RiCheckboxCircleLine />}>
       {locale?.['success'] || 'Success'}
     </ToolButton>
   );
