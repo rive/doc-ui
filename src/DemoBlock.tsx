3e183ad906f159de33a725e8d9b70e75d5a6a5a6
Guo Yunhe <i@guoyunhe.me>
2024-01-07 23:50:32 +0800

    device switcher

--- src/DemoBlock.tsx
+++ src/DemoBlock.tsx
@@ -1,16 +1,17 @@
 import { RiMacbookLine, RiSmartphoneLine, RiTabletLine } from '@remixicon/react';
 import cn from 'classnames';
-import { CSSProperties, useState } from 'react';
+import { CSSProperties, useMemo, useState } from 'react';
 import { LiveEditor, LiveError, LivePreview, LiveProvider } from 'react-live';
 import './DemoBlock.css';
 import { CopyButton } from './private/CopyButton';
 import { FileName } from './private/FileName';
 import { Spacer } from './private/Spacer';
 import { Toolbar } from './private/Toobar';
+import { ToolButton } from './private/ToolButton';
 import { ToolSelect } from './private/ToolSelect';
 
 export interface DemoBlockProps {
-  device?: 'desktop' | 'tablet' | 'mobile' | 'responsive';
+  device?: 'laptop' | 'tablet' | 'mobile' | 'responsive';
   language?: 'jsx' | 'tsx';
   filename?: string | undefined;
   code: string;
@@ -23,25 +24,42 @@ export function DemoBlock({
   code,
   filename,
   language = 'jsx',
+  device = 'responsive',
   disablePadding,
   className,
   style,
 }: DemoBlockProps) {
-  const [selectedDevice, setSelectedDevice] = useState('mobile');
+  const deviceList = useMemo(
+    () => [
+      { value: 'mobile', label: 'Mobile', icon: <RiSmartphoneLine /> },
+      { value: 'tablet', label: 'Tablet', icon: <RiTabletLine /> },
+      { value: 'laptop', label: 'Laptop', icon: <RiMacbookLine /> },
+    ],
+    []
+  );
+
+  const [selectedDevice, setSelectedDevice] = useState<string>(
+    device === 'responsive' ? 'laptop' : device
+  );
+
+  const selectedDeviceObj = useMemo(
+    () => deviceList.find((item) => item.value === selectedDevice),
+    [selectedDevice, deviceList]
+  );
+
   return (
-    <div className={cn('rive-demo-block', className)} style={style}>
+    <div
+      className={cn('rive-demo-block', 'rive-demo-block-' + selectedDevice, className)}
+      style={style}
+    >
       <Toolbar>
         <FileName language={language} filename={filename} />
         <Spacer />
-        <ToolSelect
-          value={selectedDevice}
-          onChange={setSelectedDevice}
-          options={[
-            { value: 'mobile', label: 'Mobile', icon: <RiSmartphoneLine /> },
-            { value: 'tablet', label: 'Tablet', icon: <RiTabletLine /> },
-            { value: 'laptop', label: 'Laptop', icon: <RiMacbookLine /> },
-          ]}
-        />
+        {device === 'responsive' ? (
+          <ToolSelect value={selectedDevice} onChange={setSelectedDevice} options={deviceList} />
+        ) : (
+          <ToolButton icon={selectedDeviceObj?.icon}>{selectedDeviceObj?.label}</ToolButton>
+        )}
         <CopyButton code={code} />
       </Toolbar>
       <LiveProvider
